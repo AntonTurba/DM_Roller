ad9483534e695490d32a224ba641398acfc46024
AntonTurba <anton.turba@mymail.nwtc.edu>
2018-10-22 17:25:41 -0500

    Add files via upload

---
+++ src/Attack.java
@@ -0,0 +1,18 @@
+
+public class Attack {
+	
+	private String name;
+	
+	private int numOfDice;
+	
+	private int typeOfDice;
+	
+	private int diceModifier;
+	
+	public Attack(String name, int numOfDice, int typeOfDice, int diceModifier) {
+		this.name = name;
+		this.numOfDice = numOfDice;
+		this.typeOfDice = typeOfDice;
+		this.diceModifier = diceModifier;
+	}
+}
---
+++ src/Opponent.java
@@ -0,0 +1,28 @@
+
+
+public class Opponent {
+	
+	private String name;
+	
+	private String size;
+	
+	private int armorClass;
+	
+	private int healthPoints;
+	
+	private Attack attackOne;
+	
+	private Attack attackTwo;
+	
+	public Opponent(String name, String size, int ac, int hp, Attack one) {
+		this.name = name;
+		this.size = size;
+		this.armorClass = ac;
+		this.healthPoints = hp;
+		this.attackOne = 
+	}
+	
+	public Opponent(String name, String size, int ac, int hp, Attack one, Attack two) {
+		
+	}
+}
